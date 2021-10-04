9d8bf9a84a8c1e97ad2a4ccd407104432015aba9
Shavonne Holton <shavonne.holton@gmail.com>
2021-10-04 12:50:39 -0700

    debugged db

--- db.js
+++ db.js
@@ -2,13 +2,11 @@ const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    protocol: 'postgres',
     dialectOptions: {
         ssl: {
             require: true,
             rejectUnauthorized: false,
         }
-
     }
 }
 )
